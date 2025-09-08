dd2aa8a4c14afc23dc4dff3b06839537b3457ca8
turnoxok <turnoxok@gmail.com>
2025-09-08 11:57:58 -0300

    Update responderInvitacion.js

--- netlify/functions/responderInvitacion.js
+++ netlify/functions/responderInvitacion.js
@@ -5,7 +5,7 @@ module.exports.handler = async function(event, context) {
   try {
     const { idEvento, estado } = JSON.parse(event.body);
 
-    const res = await fetch('https://script.google.com/macros/s/AKfycbwIEIglUxRi9WHE3Dp-RJFSsiQDSKb25IMvh6rGxvvnGEvxGBeD-hsHd6u6cQ7B-Z4f/exec', {
+    const res = await fetch('https://script.google.com/macros/s/AKfycbwORLG74do170DE12HT5yX4UQOA_wnFCwGLfEU-HLQPtvc1KL601ZOlEQBcqC8MhFnt/exec', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
