c60a6e3672ecdee00fa99797f6c1fdb4d72e1ca8
turnoxok <turnoxok@gmail.com>
2025-09-10 09:41:31 -0300

    Update registrarClub.js

--- netlify/functions/registrarClub.js
+++ netlify/functions/registrarClub.js
@@ -4,7 +4,7 @@ exports.handler = async function(event, context) {
   try {
     const body = JSON.parse(event.body);
 
-    const res = await fetch("https://script.google.com/macros/s/AKfycby3-TFBkxd5bso4MHx0aa-lzR6EEjuqM5e7DZFcYetucj-_Bq_zdaY-2voJquBvY-Nv/exec", {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbyOtMf2865kYSQ-0kuUBkKqk9zA08TNcoW9KQTjVa7cTwFR0hpCyPqrkgh6FjjzSFAo/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body)
