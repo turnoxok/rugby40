0b21e4ccea4b833fc41a8f43aab97f394486bb67
turnoxok <turnoxok@gmail.com>
2025-09-10 08:29:50 -0300

    Update listarEventos.js

--- netlify/functions/listarEventos.js
+++ netlify/functions/listarEventos.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch'); // solo si Node < 20
 
 module.exports.handler = async function(event, context) {
   try {
-    const res = await fetch('https://script.google.com/macros/s/AKfycbwORLG74do170DE12HT5yX4UQOA_wnFCwGLfEU-HLQPtvc1KL601ZOlEQBcqC8MhFnt/exec', {
+    const res = await fetch('https://script.google.com/macros/s/AKfycby3-TFBkxd5bso4MHx0aa-lzR6EEjuqM5e7DZFcYetucj-_Bq_zdaY-2voJquBvY-Nv/exec', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ action: 'listarEventos' })
