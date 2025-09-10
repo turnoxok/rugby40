01f06fdd7f93cbfc5e9cf9c4adde742409c24c55
turnoxok <turnoxok@gmail.com>
2025-09-10 08:30:03 -0300

    Update crearEvento.js

--- netlify/functions/crearEvento.js
+++ netlify/functions/crearEvento.js
@@ -5,7 +5,7 @@ module.exports.handler = async function(event, context) {
   try {
     const params = JSON.parse(event.body);
 
-    const res = await fetch('https://script.google.com/macros/s/AKfycbwORLG74do170DE12HT5yX4UQOA_wnFCwGLfEU-HLQPtvc1KL601ZOlEQBcqC8MhFnt/exec', {
+    const res = await fetch('https://script.google.com/macros/s/AKfycby3-TFBkxd5bso4MHx0aa-lzR6EEjuqM5e7DZFcYetucj-_Bq_zdaY-2voJquBvY-Nv/exec', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ action: 'crearEvento', ...params })
