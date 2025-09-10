2104b7f4bc440190faf7fb9ec2ca556a4791c30c
turnoxok <turnoxok@gmail.com>
2025-09-10 09:55:13 -0300

    Update registrarClub.js

--- netlify/functions/registrarClub.js
+++ netlify/functions/registrarClub.js
@@ -4,7 +4,7 @@ exports.handler = async function(event, context) {
   try {
     const body = JSON.parse(event.body);
 
-    const res = await fetch("https://script.google.com/macros/s/AKfycbyOtMf2865kYSQ-0kuUBkKqk9zA08TNcoW9KQTjVa7cTwFR0hpCyPqrkgh6FjjzSFAo/exec", {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbypWNlDdOAcFd-h9f4XLhga-PQjjcV_vhVDybHUtAx3Vyw-MrFxqT_sGwVQ2x3ppLqF/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body)
